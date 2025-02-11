e599cc23ec0f3ec2f14b61edf40172be06565aee
Fernandogbuzon <153734793+Fernandogbuzon@users.noreply.github.com>
2025-02-11 13:32:01 +0100

    Añadir logo

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MainScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('DiarioPersonal/Images/logoDiario.png', width: 100, height: 100),
+            Image.asset('assets/images/logoDiario.png', width: 100, height: 100),
             ElevatedButton(
               onPressed: () {
                 // Navegar a la pantalla de inicio de sesión

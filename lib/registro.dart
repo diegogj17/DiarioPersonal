130f3a70ac2a9e68b56ae9a83c1060705ca167c4
Diego <diegogj17@hotmail.es>
2025-02-21 15:38:46 +0100

    falta un pasito para que todo funcione

--- lib/registro.dart
+++ lib/registro.dart
@@ -26,7 +26,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => InfoScreen(email: _emailController.text.trim()),
+            builder: (context) => InfoScreen(email: _emailController.text.trim()patata),
           ),
         );
       } catch (e) {
